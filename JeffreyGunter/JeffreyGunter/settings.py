42429127949ea46fc0a7ea74e6f6a9ecaf54b689
Jeff Gunter <gunter.jeffm@gmail.com>
2017-03-03 09:44:01 -0700

    Updated settings.py

--- JeffreyGunter/JeffreyGunter/settings.py
+++ JeffreyGunter/JeffreyGunter/settings.py
@@ -12,17 +12,18 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 import os
 import dj_database_url
 from boto.s3.connection import S3Connection
-try:
-    from .secret import *
-except Exception as e:
-    SECRET_KEY=os.environ['SECRET_KEY']
+#try:
+#    from .secret import *
+#except Exception as e:
+
+SECRET_KEY=os.environ['SECRET_KEY']
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # Application definition
 INSTALLED_APPS = [
