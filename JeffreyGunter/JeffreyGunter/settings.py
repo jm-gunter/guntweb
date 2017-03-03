c45546d9292f1097564019536a4e835d90dc8301
Jeff Gunter <gunter.jeffm@gmail.com>
2017-03-03 09:30:14 -0700

    Updated settings.py

--- JeffreyGunter/JeffreyGunter/settings.py
+++ JeffreyGunter/JeffreyGunter/settings.py
@@ -11,10 +11,11 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import os
 import dj_database_url
+from boto.s3.connection import S3Connection
 try:
     from .secret import *
 except Exception as e:
-    SECRET_KEY=os.environ.get('SECRET_KEY')
+    SECRET_KEY=S3Connection(os.environ['SECRET_KEY'])
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
